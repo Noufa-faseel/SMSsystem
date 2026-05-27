c7a1fad8a17e4988cf30026e245a6a186489ca56
Noufa-faseel <tnoufa009@gmail.com>
2026-05-27 15:00:18 +0530

    added

---
+++ Backend/model/studentModel.js
@@ -0,0 +1,14 @@
+import  mongoose from 'mongoose'
+
+
+const studentSchema =new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    rollNo:{type:String,required:true},
+    className:{type:String,required:true},
+    contact:{type:String,required:true},
+
+
+
+},{timestamps:true})
+
